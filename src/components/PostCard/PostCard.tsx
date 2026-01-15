d85140b32d8877dabc1e04551f4ce5acfacc5173
pururu <wjdwo2808@gmail.com>
2026-01-15 15:59:54 +0900

    posts,postcard,commentsection, useconmments edit

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,19 +1,20 @@
-import styles from "./PostCard.module.css";
-import { Post } from "../../types";
-
+import type { Post } from '../../types';
+import LikeButton from '../LikeButton/LikeButton';
+import CommentSection from '../CommentSection/CommentSection';
+import styles from './PostCard.module.css';
 interface PostCardProps {
   post: Post;
 }
 function PostCard({ post }: PostCardProps) {
   const timeAgo = (date: string) => {
     const seconds = Math.floor((Date.now() - new Date(date).getTime()) / 1000);
-
     if (seconds < 60) return `${seconds}초 전`;
     if (seconds < 3600) return `${Math.floor(seconds / 60)}분 전`;
-    if (seconds > 86400) return `${Math.floor(seconds / 3600)}시간 전`;
-    return `${Math.floor(seconds / 86400)}`;
+    if (seconds < 86400) return `${Math.floor(seconds / 3600)}시간 전`;
+    return `${Math.floor(seconds / 86400)}일 전`;
   };
-
+  const currentUserId = 1;
+  const isLiked = post.likedBy.includes(currentUserId);
   return (
     <article className={styles.card}>
       <div className={styles.header}>
@@ -27,17 +28,19 @@ function PostCard({ post }: PostCardProps) {
           <span className={styles.time}>{timeAgo(post.createdAt)}</span>
         </div>
       </div>
-
       <p className={styles.content}>{post.content}</p>
-
       {post.image && (
         <img src={post.image} alt="" className={styles.postImage} />
       )}
-
       <div className={styles.footer}>
-        <button className={styles.actionBtn}>❤️ {post.likes}</button>
-        <button className={styles.actionBtn}>💬 {post.commentCount}</button>
+        <LikeButton
+          postId={post.id}
+          likes={post.likes}
+          isLiked={isLiked}
+        />
       </div>
+      <CommentSection postId={post.id} />
     </article>
   );
 }
+export default PostCard;
---
+++ src/hooks/useComments.ts
@@ -0,0 +1,27 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { createComment, fetchComments } from "../api/posts";
+
+
+export function useComments(postId: number) {
+    return useQuery({
+        queryKey: ['comments', postId],
+        queryFn: () => fetchComments(postId)
+    });
+}
+
+export function useCreateComment() {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({ postId, content }: {postId: number; content: string}) =>
+            createComment(postId, content),
+
+        onSuccess: (data, variables) => {
+            queryClient.invalidateQueries({
+                queryKey: ['comments', variables.postId]
+            });
+
+            queryClient.invalidateQueries({ queryKey: ['posts'] });
+        }
+    })
+}
